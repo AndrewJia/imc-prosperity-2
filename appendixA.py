c9dbf769caa6b6d899afc835f34a152f4c0d1218
Andrew Jia <andrew.jia.2013@gmail.com>
2024-03-30 00:13:52 -0700

    update algo 2000 pnl
    trader still broken maybe
    traderData can't be undefined

--- appendixA.py
+++ appendixA.py
@@ -6,38 +6,27 @@ class Trader:
     
     def run(self, state: TradingState):
         # Only method required. It takes all buy and sell orders for all symbols as an input, and outputs a list of orders to be sent
-        #print("traderData: " + state.traderData)
-        #print("Observations: " + str(state.observations))
+        print("traderData: " + state.traderData)
+        print("Observations: " + str(state.observations))
         result = {}
         for product in state.order_depths:
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
-            
-            #print("Acceptable price : " + str(acceptable_price))
-            print(type(product))
-            #print("product" + product)
-            print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
-
+            acceptable_price = 10;  # Participant should calculate this value
             if product == 'AMETHYSTS':
-                acceptable_price_buy = 10000
-                acceptable_price_sell = 10000
-                print("buying amethysts at "+str(acceptable_price_buy))
-            else:
-                acceptable_price_buy = 10
-                acceptable_price_sell = 10
-                continue
+                acceptable_price = 10000
+            print("Acceptable price : " + str(acceptable_price))
+            print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
     
             if len(order_depth.sell_orders) != 0:
                 best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                print("best ask is "+str(best_ask_amount))
-                if int(best_ask) < acceptable_price_buy:
+                if int(best_ask) < acceptable_price:
                     print("BUY", str(-best_ask_amount) + "x", best_ask)
                     orders.append(Order(product, best_ask, -best_ask_amount))
     
             if len(order_depth.buy_orders) != 0:
                 best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                print("best bid is "+str(best_bid_amount))
-                if int(best_bid) > acceptable_price_sell:
+                if int(best_bid) > acceptable_price:
                     print("SELL", str(best_bid_amount) + "x", best_bid)
                     orders.append(Order(product, best_bid, -best_bid_amount))
             
