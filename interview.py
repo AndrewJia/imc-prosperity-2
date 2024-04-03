7f46da6eaf65c1a0fe3fa8660e30513e9e5f4738
Andrew Jia <andrew.jia.2013@gmail.com>
2024-04-02 17:24:53 -0700

    xdd

---
+++ interview.py
@@ -0,0 +1,5 @@
+##you have a community with people A, B, C, D, G, E, F. 
+#A knows B, A knows C, D knows E, C knows F, F knows G
+#all connections are bidirectional 
+#problem1: what is the data structure, code it
+#Problem2: count lone pears
