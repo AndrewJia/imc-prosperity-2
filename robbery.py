1a1371c2840042386e124266ca4821f590fa4f89
Andrew Jia <andrew.jia.2013@gmail.com>
2024-04-08 13:08:22 -0700

    Update robbery.py

--- robbery.py
+++ robbery.py
@@ -1,14 +1,100 @@
-from datamodel import OrderDepth, UserId, TradingState, Order
-from typing import List
+from datamodel import Listing, Observation, Order, OrderDepth, ProsperityEncoder, Symbol, Trade, TradingState
+from typing import Any
 import string
+import json
 
 import collections
 from collections import defaultdict
 
+## logger class for JMerle's visualizer
+class Logger:
+    def __init__(self) -> None:
+        self.logs = ""
+
+    def print(self, *objects: Any, sep: str = " ", end: str = "\n") -> None:
+        self.logs += sep.join(map(str, objects)) + end
+
+    def flush(self, state: TradingState, orders: dict[Symbol, list[Order]], conversions: int, trader_data: str) -> None:
+        print(json.dumps([
+            self.compress_state(state),
+            self.compress_orders(orders),
+            conversions,
+            trader_data,
+            self.logs,
+        ], cls=ProsperityEncoder, separators=(",", ":")))
+
+        self.logs = ""
+
+    def compress_state(self, state: TradingState) -> list[Any]:
+        return [
+            state.timestamp,
+            state.traderData,
+            self.compress_listings(state.listings),
+            self.compress_order_depths(state.order_depths),
+            self.compress_trades(state.own_trades),
+            self.compress_trades(state.market_trades),
+            state.position,
+            self.compress_observations(state.observations),
+        ]
+
+    def compress_listings(self, listings: dict[Symbol, Listing]) -> list[list[Any]]:
+        compressed = []
+        for listing in listings.values():
+            compressed.append([listing["symbol"], listing["product"], listing["denomination"]])
+
+        return compressed
+
+    def compress_order_depths(self, order_depths: dict[Symbol, OrderDepth]) -> dict[Symbol, list[Any]]:
+        compressed = {}
+        for symbol, order_depth in order_depths.items():
+            compressed[symbol] = [order_depth.buy_orders, order_depth.sell_orders]
+
+        return compressed
+
+    def compress_trades(self, trades: dict[Symbol, list[Trade]]) -> list[list[Any]]:
+        compressed = []
+        for arr in trades.values():
+            for trade in arr:
+                compressed.append([
+                    trade.symbol,
+                    trade.price,
+                    trade.quantity,
+                    trade.buyer,
+                    trade.seller,
+                    trade.timestamp,
+                ])
+
+        return compressed
+
+    def compress_observations(self, observations: Observation) -> list[Any]:
+        conversion_observations = {}
+        for product, observation in observations.conversionObservations.items():
+            conversion_observations[product] = [
+                observation.bidPrice,
+                observation.askPrice,
+                observation.transportFees,
+                observation.exportTariff,
+                observation.importTariff,
+                observation.sunlight,
+                observation.humidity,
+            ]
+
+        return [observations.plainValueObservations, conversion_observations]
+
+    def compress_orders(self, orders: dict[Symbol, list[Order]]) -> list[list[Any]]:
+        compressed = []
+        for arr in orders.values():
+            for order in arr:
+                compressed.append([order.symbol, order.price, order.quantity])
+
+        return compressed
+
+logger = Logger()
+
 
 class Trader:
 
-    starfruit_cache = []
+    starfruit_cache = [5000,5000,5000,5000]
     starfruit_dim = 4
 
     #robbery
@@ -29,14 +115,14 @@ class Trader:
         # Only method required. It takes all buy and sell orders for all symbols as an input, and outputs a list of orders to be sent
         #print("traderData: " + state.traderData)
         #print("Observations: " + str(state.observations))
-        print(state.position)
+        #print(state.position)
         result = {}
         for product in state.order_depths:
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
             
             
-            print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
+            logger.print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
 
             if product in state.position:
                 current_position = state.position[product]
@@ -53,6 +139,11 @@ class Trader:
                     acceptable_price_sell = 10000
                 
             else:
+                s_price = self.calc_next_price_starfruit()
+                logger.print(self.starfruit_cache, "STARFRUIT PRICED AT ", s_price)
+                acceptable_price_buy = s_price - 1
+                acceptable_price_sell = s_price + 1
+
                 if len(self.starfruit_cache) == self.starfruit_dim:
                     self.starfruit_cache.pop(0)
 
@@ -61,24 +152,21 @@ class Trader:
 
                 self.starfruit_cache.append((best_ask+best_bid)/2)
 
-                s_price = self.calc_next_price_starfruit()
-                print("STARFRUIT PRICED AT ", s_price)
-                acceptable_price_buy = s_price - 1
-                acceptable_price_sell = s_price + 1
+
 
             
             for (ask_price, ask_amount) in list(order_depth.sell_orders.items()):
                 if int(ask_price) <= acceptable_price_buy:
-                    print("BUY", product, str(-ask_amount) + "x", ask_price)
-                    #orders.append(Order(product, ask_price, -ask_amount))
-                    orders.append(Order(product, ask_price, 20-current_position))
+                    logger.print("BUY", product, str(-ask_amount) + "x", ask_price)
+                    orders.append(Order(product, ask_price, -ask_amount))
+                    #orders.append(Order(product, ask_price, 20-current_position))
                     break
 
             for (bid_price, bid_amount) in list(order_depth.buy_orders.items()):
                 if int(bid_price) >= acceptable_price_sell:
-                    print("SELL", product, str(bid_amount) + "x", bid_price)
-                    #orders.append(Order(product, bid_price, -bid_amount))
-                    orders.append(Order(product, bid_price, -20-current_position))
+                    logger.print("SELL", product, str(bid_amount) + "x", bid_price)
+                    orders.append(Order(product, bid_price, -bid_amount))
+                    #orders.append(Order(product, bid_price, -20-current_position))
                     break
             
             result[product] = orders
@@ -86,5 +174,6 @@ class Trader:
     
         traderData = "SAMPLE" # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
         
-        conversions = 1
+        conversions = 0
+        logger.flush(state, result, conversions, traderData)
         return result, conversions, traderData
