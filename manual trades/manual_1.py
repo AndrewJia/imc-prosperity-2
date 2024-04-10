9508136d83b5b51752a6e70b6e630a46b35118cd
Andrew Jia <andrew.jia.2013@gmail.com>
2024-04-09 18:34:52 -0700

    Update manual_1.py
    decimal prices
    (result unchanged)

--- manual trades/manual_1.py
+++ manual trades/manual_1.py
@@ -1,8 +1,11 @@
 max = 0
+
+#print([x / 10.0 for x in range(0, 1000)])
+
 for bid1 in range(100):
     for bid2 in range(bid1+1, 100):
         profit = 0
-        for fish in range(100):
+        for fish in [x / 10.0 for x in range(0, 1000)]:
             if fish >= bid2:
                 profit += bid2
             elif fish >= bid1:
