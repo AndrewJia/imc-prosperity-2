987b6d3f26dcb64eceea5dfbe501bd7ea619d3d6
Andrew Jia <andrew.jia.2013@gmail.com>
2024-04-12 13:48:22 -0700

    update manual 2 solution

--- manual trades/manual_2.java
+++ manual trades/manual_2.java
@@ -7,22 +7,31 @@ import java.util.*;
 
 public class manual_2 {
     public static void main(String[] args) {
-        double[][] dp = new double[4][6];    //4 currencies, 5 trades
+        double[][] dp = new double[4][7];    //4 currencies, 5 trades. cols 0 and 6 are start/end shells
         dp[3][0] = 100.0; //starting 100
 
         for(int c = 1; c <= 5; c++) {
+            System.out.println("--------------- round "+ c + " ---------------");
+
+            //shells
+            System.out.print("best shells comes from ");
+            dp[3][c] = multiMax(0.71*dp[0][c-1], 1.56*dp[1][c-1], 0.46*dp[2][c-1], 1.00*dp[3][c-1]);
+            if (c==5) break;
+
             //pizza
+            System.out.print("best pizza comes from ");
             dp[0][c] = multiMax(1.00*dp[0][c-1], 2.05*dp[1][c-1], 0.64*dp[2][c-1], 1.41*dp[3][c-1]);
             //wasabi
+            System.out.print("best wasabi comes from ");
             dp[1][c] = multiMax(0.48*dp[0][c-1], 1.00*dp[1][c-1], 0.30*dp[2][c-1], 0.61*dp[3][c-1]);
             //snowball
+            System.out.print("best snowball comes from ");
             dp[2][c] = multiMax(1.52*dp[0][c-1], 3.26*dp[1][c-1], 1.00*dp[2][c-1], 2.08*dp[3][c-1]);
-            //shells
-            dp[3][c] = multiMax(0.71*dp[0][c-1], 1.56*dp[1][c-1], 0.46*dp[2][c-1], 1.00*dp[3][c-1]);
+            
         }
-        System.out.println(dp[3][5]); //print max ending shells
+        
         for(int r = 0; r < 4; r++) {
-            for(int c = 0; c < 5; c++) {
+            for(int c = 0; c < 6; c++) {
                 System.out.print(String.format("%,06.2f", Double.valueOf(dp[r][c])) + " ");
             }
             System.out.println();
@@ -31,7 +40,13 @@ public class manual_2 {
 
     // self explanatory
     public static double multiMax(double a, double b, double c, double d) {
-        return Math.max(Math.max(a, b), Math.max(c, d));
+        double result = Math.max(Math.max(a, b), Math.max(c, d));
+        if(result == a) System.out.println("pizza");
+        if(result == b) System.out.println("wasabi");
+        if(result == c) System.out.println("snowball");
+        if(result == d) System.out.println("shells");
+
+        return result;
     }
 }
 
